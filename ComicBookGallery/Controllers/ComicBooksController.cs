9f486fea50ab1fb0e18e1e364fb61db853c46b92
Naval Kulkarni <navalkulkarni@gmail.com>
2019-05-18 06:51:44 +0530

    added comic books index view and method in controller

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -1,4 +1,5 @@
-﻿using ComicBookGallery.Models;
+﻿using ComicBookGallery.Data;
+using ComicBookGallery.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,27 +9,22 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
+        public ActionResult Detail(int? id)
         {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue!Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Favorite = true,
-                Artists = new Artist[]
-                {
-                    new Artist { Name="Dan Slott",Role="Script" },
-                    new Artist { Name="Humberto Ramos",Role="Pencils" },
-                    new Artist { Name= "Victor Olazaba",Role="Inks" },
-                    new Artist { Name="Edgar Delgado",Role="Colors" },
-                    new Artist { Name="Chris Eliopoulos",Role="Letters" },
-                }
-            };
+            var comicBook = new ComicBook();
+            if (id == null)
+                return HttpNotFound();
+            else
+                comicBook = ComicBookRepository.GetComicBook(id.Value);
 
             
             return View(comicBook);
         }
 
+        public ActionResult Index()
+        {
+            var comicBooks = ComicBookRepository.GetComicBooks();
+            return View(comicBooks);
+        }
     }
 }
